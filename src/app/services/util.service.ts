f14794d950ef2a6da037510317638eda841b67bc
arjunbaru <akshaybhat301094@gmail.com>
2021-06-11 22:39:30 +0530

    added jsdoc comments

--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -7,6 +7,13 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 export class UtilService {
   constructor() {}
 
+  /**
+   * function used to sort array of objects in ascending order
+   * @param obj1 first object
+   * @param obj2 second object
+   * @param key sorting based on key
+   * @returns sorting order ascending
+   */
   public ascending(obj1: any, obj2: any, key: string): number {
     const first = obj1[key].toUpperCase();
     const second = obj2[key].toUpperCase();
@@ -19,6 +26,13 @@ export class UtilService {
     return 0;
   }
 
+  /**
+   * function used to sort array of objects in descending order
+   * @param obj1 first object
+   * @param obj2 second object
+   * @param key sorting based on key
+   * @returns sorting order descending
+   */
   public descending(obj1: any, obj2: any, key: string): number {
     const first = obj1[key].toUpperCase();
     const second = obj2[key].toUpperCase();
@@ -31,6 +45,11 @@ export class UtilService {
     return 0;
   }
 
+  /**
+   * function used to add any missing keys from source to target object
+   * @param source source object
+   * @param target target object
+   */
   public addMissingValues(source: any, target: any): void {
     for (const key in source) {
       if (!target[key] && key !== 'editMode') {
